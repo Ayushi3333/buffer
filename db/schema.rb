ac83007a16d4e686050bb26bc94831ce9d0aed58
ayushi gupta <ayushi3101gupta@gmail.com>
2020-11-30 18:19:46 +0100

    Add Review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_30_165830) do
+ActiveRecord::Schema.define(version: 2020_11_30_171531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,17 @@ ActiveRecord::Schema.define(version: 2020_11_30_165830) do
     t.index ["user_id"], name: "index_buddies_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.integer "rating"
+    t.bigint "user_id", null: false
+    t.bigint "buddy_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["buddy_id"], name: "index_reviews_on_buddy_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -40,4 +51,6 @@ ActiveRecord::Schema.define(version: 2020_11_30_165830) do
   end
 
   add_foreign_key "buddies", "users"
+  add_foreign_key "reviews", "buddies"
+  add_foreign_key "reviews", "users"
 end
