e46e175c8639639768fda82d36cef7533d6bb434
patrickminero <patrickminerovalencia@gmail.com>
2020-12-01 15:52:56 +0100

    added images to cards in index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def show_buddy_photo(buddy)
+    if buddy.photo.attached?
+       cl_image_tag buddy.photo.key, :height => 300, :class => "card-img-top"
+    else
+      image_tag('placeholder.jpg')
+    end
+  end
 end
