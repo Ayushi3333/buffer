9c4a36cc0c805913c53a2168b27dbb7fc9adefe4
Malik Soukri <malik.soukri@gmail.com>
2020-12-01 11:43:08 +0100

    Seeds 10 buddies

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ 
+require 'faker'
+
+user = User.create!({
+  email: "test@email.come",
+  password: "123456"
+})
+
+
+10.times do
+  buddies = Buddy.create!(
+    name: Faker::Artist.name,
+    description: Faker::Quote.famous_last_words,
+    # photo: https://picsum.photos/200/300
+    availability: true,
+    user_id: user.id
+  )
+end
+
+
+
+
+
+
+
