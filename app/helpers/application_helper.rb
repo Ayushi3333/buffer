6fb3b631390b424b971dbebbb5a21a0daed85a64
Malik Soukri <malik.soukri@gmail.com>
2020-12-02 14:39:18 +0100

    Show page and Index working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,30 @@ module ApplicationHelper
       
     end
   end
+
+  def display_tag(tag)
+    case tag
+    when "Diplomatic"
+      return "Will argue your case better than the best layer in town if you run into trouble"
+    when "Shredded"
+      return "Will intimidate anyone who want to pick a fight with you, don’t be afraid to dance alone!"
+    when "Possessive"
+      return "Will keep any interested party away from you. Your soulmate is safe!"
+    when "Motherly"
+      return "Will ensure you keep your clothes on. Manners first!"
+    when "Comforting"
+      return "Always a tissue in his pocket, don’t be afraid to cry!"
+    when "Tough love"
+      return "When you just need a wakeup call before calling your ex."
+    when "Social"
+      return "When you don’t want to look like you came alone on the next party"
+    when "Memory"
+      return "Will whisper you the name of people you don’t remember, don’t feel ashamed."
+    when "Designated driver"
+      return "will make sure to take you back home safe, give him your address and feel free to forget!"
+    when "Wing Man"
+      return "Someone that will help you get lucky, you will star in every circumstances!"
+    end
+  end
+  
 end
