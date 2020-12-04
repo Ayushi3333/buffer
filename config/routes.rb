cf8903cb553ed123f601bfcda4f03a1d0bd4549f
ayushi gupta <ayushi3101gupta@gmail.com>
2020-12-04 10:54:27 +0100

    Add bookings index page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :buddies do
     get 'bookings/checkout', to: "bookings#checkout", as: :checkout
-    resources :bookings, only: [:new, :create, :index, :show]
+    resources :bookings, only: [:new, :create, :index, :show, :destroy]
 end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
